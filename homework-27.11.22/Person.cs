0e08e8934facf5db37286c23077b15f22ac585a6
alexberg90 <ayazya@code.edu.az>
2022-11-27 14:48:58 +0400

    home-work ID

---
+++ homework-27.11.22/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace homework_27._11._22
+{
+    internal class Person
+    {
+        public static int ID = 0;
+        
+        public static int MyProperty { get; set; }
+        public Person()
+        {
+            ID++;
+           
+        }
+    }
+}
---
+++ homework-27.11.22/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homework_27._11._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Person person = new Person();
+            Person person2 = new Person();
+            Person person3 = new Person();
+
+
+
+
+            Console.WriteLine(Person.ID);
+        }
+    }
+}
